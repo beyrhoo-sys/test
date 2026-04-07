706aaaeb82ea7f84cbc7be7813fb0c40f55300f8
beyrhoo-sys <mohammadadhwahazim@gmail.com>
2026-04-07 18:32:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-fetch("data.json")
+fetch('lovely-settings.json')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById('output').textContent = JSON.stringify(data, null, 2);
+  });
   .then(response => response.json())
   .then(data => {
     document.getElementById("output").textContent = JSON.stringify(data, null, 2);
